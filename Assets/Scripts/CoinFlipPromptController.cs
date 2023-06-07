7a46a3acbe7504f89cb4688e2b271f1d64a90013
musicman928 <musicman928201@gmail.com>
2023-06-06 22:34:20 -0600

    Temporary Save

--- Assets/Scripts/CoinFlipPromptController.cs
+++ Assets/Scripts/CoinFlipPromptController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CoinFlipPromptController : MonoBehaviour
-{
+public class CoinFlipPromptController : MonoBehaviour {
+    public Button HeadsButton;
+    public Button TailsButton;
+    public Transform Coin;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
